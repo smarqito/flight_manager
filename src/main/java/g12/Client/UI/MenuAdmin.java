74d9195e9a36a6e7fa2177a6d91084107fc5514f
José Malheiro <jafmalheiro@live.com.pt>
2022-01-06 17:44:13 +0000

    menus admin/cliente started

--- src/main/java/g12/Client/UI/MenuAdmin.java
+++ src/main/java/g12/Client/UI/MenuAdmin.java
@@ -9,12 +9,23 @@ public class MenuAdmin {
 
     }
 
+    /**
+     * Método para apresentar o menu principal ao administrador
+     */
     public void menuAdmin() {
         
         Menu menu = new Menu(new String[]{
             "Registar voo",
             "Encerrar dia"
         });
+
+        // Registar os handlers
+
+        menu.run();
     }
 
+
+    public void menuRegistarVoo(){
+        
+    }
 }
--- src/main/java/g12/Client/UI/MenuCliente.java
+++ src/main/java/g12/Client/UI/MenuCliente.java
@@ -1,5 +1,18 @@
 package g12.Client.UI;
 
 public class MenuCliente {
+
+    /**
+     * Construtor para o menu a apresentar ao cliente 
+     */
+    public MenuCliente() {
     
+    }
+    
+    /**
+     * Método para apresentar o menu principal para um cliente
+     */
+    public void menuCliente(){
+
+    }
 }
