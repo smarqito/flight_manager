9c53d6c5e0a508241e38142ee36837752189895d
Marco António Sousa <smarqito@gmail.com>
2022-01-06 15:13:29 +0000

    client menus

--- src/main/java/g12/Client/Client.java
+++ src/main/java/g12/Client/Client.java
@@ -30,5 +30,44 @@ public class Client {
 			e.printStackTrace();
 		}
 	}
+	/**
+	 * Inicio [sem login]:
+	 * 1. login
+	 * 2. registar
+	 * 
+	 * 1: Login
+	 * 1.1. Pedir user
+	 * 1.2 Pedir pass
+	 * 
+	 * 2: Registar
+	 * 2.1 Pedir user
+	 * 2.2 Pedir pass
+	 * 
+	 * Homepage [com login]:
+	 * 1. Cliente
+	 * 2. Admin
+	 * 
+	 * 1. Cliente : Homepage
+	 * 1.1 Reservar Voo
+	 * 1.2 Cancelar Voo
+	 * 
+	 * 1.1. : Reservar Voo
+	 * 1.1.1 Ver voos disponiveis
+	 * 1.1.2 Fazer reserva
+	 * 
+	 * 1.1.2 : Fazer reserva
+	 * 1.1.2.1 Pedir percurso, separados por virgula
+	 * 1.1.2.2 Pedir data minima
+	 * 1.1.2.3 Pedir data maxima
+	 * 
+	 * 2. Admin : Homepage
+	 * 2.1 Registar Voo
+	 * 2.2 Encerrar dia
+	 * 
+	 * 2.1 : Registar Voo
+	 * 2.1.1 Pedir origem
+	 * 2.1.2 Pedir destino
+	 * 2.1.3 Pedir capacidade
+	 */
 
 }
