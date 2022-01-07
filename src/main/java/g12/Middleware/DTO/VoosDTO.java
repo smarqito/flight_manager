e36597491842553c7e03c19a840f56a6c9df10cf
Miguel Fernandes <miguelffernandes13@gmail.com>
2022-01-07 16:35:36 +0000

    VoosDisponiveis a funcionar

---
+++ src/main/java/g12/Middleware/DTO/VoosDTO.java
@@ -0,0 +1,26 @@
+package g12.Middleware.DTO;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class VoosDTO extends ArrayList<VooDTO> {
+
+    public void serialize(DataOutputStream out) throws IOException {
+        out.writeInt(this.size());
+        for(int i = 0; i < this.size(); i++){
+            this.get(i).serialize(out);
+        }
+    }
+
+    public static VoosDTO deserialize(DataInputStream in) throws IOException {
+        int size = in.readInt();
+        VoosDTO voos = new VoosDTO();
+        for(int i = 0; i < size; i++){
+            voos.add(VooDTO.deserialize(in));
+        }
+        return voos;
+    }
+
+}
