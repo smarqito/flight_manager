9612cec29a7dab0ce1029d994c9633dfe7cf2ffc
Miguel Fernandes <miguelffernandes13@gmail.com>
2022-01-13 10:40:44 +0000

    serialize e deserialize getFlightListDTO

--- src/main/java/g12/Middleware/DTO/ResponseDTO/GetFlightListDTO.java
+++ src/main/java/g12/Middleware/DTO/ResponseDTO/GetFlightListDTO.java
@@ -4,7 +4,9 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GetFlightListDTO extends ResponseDTO{
     private List<List<String>> voos;
@@ -21,11 +23,20 @@ public class GetFlightListDTO extends ResponseDTO{
     @Override
     public void serialize(DataOutputStream out) throws IOException {
         super.serialize(out);
+        Map<String, Integer> mapa = new HashMap<>();
+        int count = 0;
         out.writeInt(voos.size());
         for (List<String> lista : voos){
             out.writeInt(lista.size());
             for (String voo : lista){
-                out.writeUTF(voo);
+                Integer v;
+                if ((v = mapa.get(voo)) == null){
+                    mapa.put(voo, count++);
+                    out.writeInt(-1);
+                    out.writeUTF(voo);
+                }else{
+                    out.writeInt(v);
+                }
             }
         }
     }
@@ -34,12 +45,21 @@ public class GetFlightListDTO extends ResponseDTO{
         int respCode = in.readInt();
         int sizeList = in.readInt();
         List<List<String>> voos = new ArrayList<>(sizeList);
+        Map<Integer, String> mapa = new HashMap<>();
+        int count = 0;
         for (int i = 0; i < sizeList; i++){
             int size = in.readInt();
             List<String> voos2 = new ArrayList<>(size);
             for (int j = 0; j < size; j++){
-                String v = in.readUTF();
-                voos2.add(v);
+                int n;
+                if ((n = in.readInt()) == -1){
+                    String v = in.readUTF();
+                    mapa.put(count++, v);
+                    voos2.add(v);
+                } else {
+                    String v = mapa.get(n);
+                    voos2.add(v);
+                }
             }
             voos.add(voos2);
         }
